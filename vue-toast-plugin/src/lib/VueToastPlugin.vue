2c2cb0df4abaa0568f9357bbe7e81f7ccf4b96ae
安志浩 <haohao809@126.com>
2018-08-30 17:52:49 +0800

    alert

--- vue-toast-plugin/src/lib/VueToastPlugin.vue
+++ vue-toast-plugin/src/lib/VueToastPlugin.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
-        <div class="toast-simple" @click='toast'>
+        <div class="toast-simple" @click='trigger'>
             <slot></slot>
         </div>
-        <section class="toast-container" v-if="show">
+        <section class="toast-container" v-if="toastType">
             <div class="toast" v-bind:class="[visible?'fade-in':'fade-out']">
-                <span>{{message}}</span>
+                <p>{{message}}</p>
             </div>
         </section>
+        <section class="toast-container" v-if="alertType">
+            <div class="toast" v-bind:class="[visible?'fade-in':'fade-out']">
+                <p>{{message}}</p>
+                <p class="btn" @click="alertBtn">
+                    <span class="btnMsg">{{alertBtnMsg}}</span>
+                </p>
+            </div>
+        </section> 
+        <section class="toast-container" v-if="confirmType">
+            <div class="toast" v-bind:class="[visible?'fade-in':'fade-out']">
+                <p>{{message}}</p>
+                <p class="btn">
+                    <span class="btnMsg">{{confirmBtnMsgOK}}</span>
+                </p>
+            </div>
+        </section>       
     </div>
 </template>
 <style lang="scss">
@@ -52,38 +68,92 @@
             animation-fill-mode: both;
         }
         .toast{
-            width: 180px;
-            height: 60px;
+            min-width: 180px;
+            min-height: 60px;
             text-align: center;
-            line-height: 60px;
             background-color: black;
             color: white;
             border-radius: 13px;
+        }
+        .btn::before{
+            content: '';
+            position: absolute;
+            z-index: 1;
+            background-color:#e5e5e5;
+            height: 1px;
+            left: 0;
+            right: 0;
+        }
+        .btnMsg{
+            display: inline-block;
+            margin-top: 15px;
         }
     }
 </style>
 <script>
     export default{
         data(){
-            return{
-                visible:true,
-                show: false
+            return {
+                visible: true,
+                toastType: false,
+                alertType: false,
+                confirmType: false
             }
         },
-        props: ['message'],
+        props: {
+            message: {
+                type: String,
+                default: ''
+            },
+            alertBtnMsg: {
+                type: String,
+                default: '确定'
+            },
+            confirmBtnMsgOK: {
+                type: String,
+                default: '确定'
+            },
+            toast:{
+                type: Boolean,
+                default: false
+            },
+            alert:{
+                type: Boolean,
+                default: false
+            },
+            confirm:{
+                type: Boolean,
+                default: false
+            }
+        },
         methods:{
-            toast(){
-                this.show = true;
+            trigger(){                
                 var that = this;
-                setTimeout(() =>{
-                    that.visible = false;
-                    setTimeout(()=>{
-                       that.show = false;
-                       that.visible = true;
-                   },1000)
-                },800)
+                if(this.toast){
+                        this.toastType = true;
+                        setTimeout(() =>{
+                            that.visible = false;
+                            setTimeout(()=>{
+                               that.toastType = false;
+                               that.visible = true;
+                           },1000)
+                    },800)
+                }else if(this.alert){
+                    this.alertType = true;
+                }else if(this.confirm){
+                    this.confirmType = true;
+                }
 
 
+
+            },
+            alertBtn(){
+                this.visible = false;
+                var that = this
+                setTimeout(()=>{
+                        that.alertType = false;
+                        that.visible = true;
+                },1000)
             }
         }
     }
